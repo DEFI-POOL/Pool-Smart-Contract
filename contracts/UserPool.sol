f5380bf8a620083646e574d45f7e3abf0acd18c1
george-shammar <george.gbenle@outlook.com>
2021-10-01 14:07:11 +0000

    Import Initializable.sol from opezeppelin

--- contracts/UserPool.sol
+++ contracts/UserPool.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/introspection/ERC165CheckerUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@pooltogether/fixed-point/contracts/FixedPoint.sol";
 
 
