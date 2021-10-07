91a45cc49ac98c49c5bcc631f8fd24b446594770
george-shammar <george.gbenle@outlook.com>
2021-10-07 10:36:12 +0000

    Import FairyTokenInterface

--- contracts/UserPool.sol
+++ contracts/UserPool.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./FairyTokenInterface.sol";
 
 
 
