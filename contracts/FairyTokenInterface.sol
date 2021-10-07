5882e7feac0d774db5eed5c354fc3c4443f753e1
george-shammar <george.gbenle@outlook.com>
2021-10-07 10:34:32 +0000

    Implement FairyTokenInterface

---
+++ contracts/FairyTokenInterface.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+abstract contract Fairy is ERC20{
+    
+    function mint(address _user, uint amount) public {}
+    
+    function Burn(address _user, uint256 _amount) external virtual {}
+}
